c3ce15daa9dba947c6d3343da47b8696758fcd26
Andreas Rohner <andy641@gmx.at>
2011-12-31 11:15:53 +0100

    Beispielseite

---
+++ templates/head.lua
@@ -0,0 +1,3 @@
+<head>
+<title><? send("It works!") ?></title>
+</head>
---
+++ www/localhost/index.lua
@@ -0,0 +1,50 @@
+<? 
+--has to come first so that set-cookie header can be sent
+local s=req:getSession() 
+
+?>
+<html>
+<? include("templates/head.lua") ?>
+<body>
+<h1><? 
+	local c=db.get("count")
+	if not c then
+		c=0
+	end
+
+	c=c+1
+
+	send("It works! Number: ", c) 
+
+	db.put("count",c)
+
+	--make change to db persistent
+	--db.sync()
+?></h1>
+
+<h2>
+<?
+	local c=s.c
+	if not c then
+		c=0
+	end
+
+	c=c+1
+	send("It works! Number: ", c) 
+
+	s.c=c
+
+?>
+</h2>
+
+
+<?
+	--open http://localhost:8080/?xss=It%20works
+	local xss=req.get.xss
+	if xss then
+		send("<h3>", xss,"</h3>") 
+	end
+?>
+
+</body>
+</html>
